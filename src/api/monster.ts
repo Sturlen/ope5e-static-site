3f3fd8acea83c5966128e75d2e9bed2e2037cef0
Sturlen <43607012+Sturlen@users.noreply.github.com>
2023-05-08 23:15:08 +0200

    add stats

--- src/api/monster.ts
+++ src/api/monster.ts
@@ -1,16 +1,38 @@
-import { z } from "zod";
+import { z } from "zod"
 
-export const MonsterSchema = z.object({
-    slug: z.string(),
-    name: z.string(),
-    size: z.string(),
-    type: z.string(),
-    armor_class: z.number(),
-    alignment: z.string(),
-    challenge_rating: z.string(),
-    actions: z.string().or(z.array(z.object({name: z.string(), desc: z.string()}))),
-    document__license_url: z.string(),
-    document__title: z.string()
-})
+export const MonsterSchema = z
+    .object({
+        slug: z.string(),
+        name: z.string(),
+        size: z.string(),
+        type: z.string(),
+        armor_class: z.number(),
+        alignment: z.string(),
+        challenge_rating: z.string(),
+        actions: z
+            .string()
+            .or(z.array(z.object({ name: z.string(), desc: z.string() }))),
+        document__license_url: z.string(),
+        document__title: z.string(),
+        strength: z.number(),
+        dexterity: z.number(),
+        constitution: z.number(),
+        intelligence: z.number(),
+        wisdom: z.number(),
+        charisma: z.number(),
+    })
+    .transform((m) => ({
+        ...m,
+        stats: {
+            str: m.strength,
+            dex: m.dexterity,
+            con: m.constitution,
+            int: m.intelligence,
+            wis: m.wisdom,
+            chr: m.charisma,
+        } as const,
+    }))
+
+export type Stat = "str" | "dex" | "con" | "int" | "wis" | "chr"
 
 export type Monster = z.infer<typeof MonsterSchema>
