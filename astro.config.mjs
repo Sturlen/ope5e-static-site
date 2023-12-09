392d7294cf59e728cde2fbf5faf5ee7be5e9e17f
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-12-09 16:37:42 +0100

    enable build cache

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/static";
+import { defineConfig } from "astro/config"
+import vercel from "@astrojs/vercel/static"
 
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  adapter: vercel({
-    analytics: true
-  }),
-  integrations: [react()]
-});
+    output: "static",
+    adapter: vercel({
+        analytics: true,
+    }),
+    experimental: {
+        contentCollectionCache: true,
+    },
+    integrations: [react()],
+})
