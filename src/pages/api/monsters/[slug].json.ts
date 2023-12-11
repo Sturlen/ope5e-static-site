d55ddcfdb068a9b06d8c59eaa0f41864725d1f14
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-12-11 20:32:18 +0100

    test json api

---
+++ src/pages/api/monsters/[slug].json.ts
@@ -0,0 +1,19 @@
+import type { APIRoute } from "astro"
+import { getCollection, getEntry } from "astro:content"
+
+export const GET: APIRoute = async ({ params, request }) => {
+    const slug = params.slug ?? "null"
+    const entry = await getEntry("monsters", slug)
+    if (!entry) {
+        return new Response("Not found", { status: 404 })
+    }
+    return new Response(JSON.stringify(entry.data))
+}
+
+export async function getStaticPaths() {
+    const monsterEntries = await getCollection("monsters")
+
+    return monsterEntries.map((monster) => ({
+        params: { slug: monster.id },
+    }))
+}
