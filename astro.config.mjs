5a860a6da32855963c208c3efe39383b936b2d51
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-12-11 20:11:48 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-import { defineConfig } from "astro/config"
-import vercel from "@astrojs/vercel/static"
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
+import react from "@astrojs/react";
 
-import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
-    adapter: vercel({
-        analytics: true,
-    }),
-    experimental: {
-        contentCollectionCache: true,
-    },
-    integrations: [react()],
-})
+  output: "static",
+  adapter: vercel({
+    analytics: true
+  }),
+  experimental: {
+    contentCollectionCache: true
+  },
+  integrations: [react(), sitemap()]
+});
