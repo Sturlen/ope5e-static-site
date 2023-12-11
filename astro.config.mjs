91e8497b548851e8c9e001177ec6bd5cb08ba025
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-12-11 20:36:09 +0100

    canonical site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/static";
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import vercel from "@astrojs/vercel/static"
+import react from "@astrojs/react"
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  adapter: vercel({
-    analytics: true
-  }),
-  experimental: {
-    contentCollectionCache: true
-  },
-  integrations: [react(), sitemap()]
-});
+    output: "static",
+    adapter: vercel({
+        analytics: true,
+    }),
+    experimental: {
+        contentCollectionCache: true,
+    },
+    site: "https://5e.spetland.no",
+    integrations: [react(), sitemap()],
+})
