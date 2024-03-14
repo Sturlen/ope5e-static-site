d860b19ea2cbc342f6e577c983951e3b48e9315f
sturlen <43607012+Sturlen@users.noreply.github.com>
2024-03-14 23:03:11 +0100

    inline

--- api/search.ts
+++ api/search.ts
@@ -2,13 +2,12 @@ import { getCollection } from "astro:content"
 
 export const prerender = false
 
-const monsters = await getCollection("monsters")
-
 export default async (request: Request) => {
     const url = new URL(request.url)
     const name = url.searchParams.get("name") ?? "Creature"
 
     if (!name) return new Response("Query must not be empty", { status: 400 })
+    const monsters = await getCollection("monsters")
 
     const entries = monsters
 
