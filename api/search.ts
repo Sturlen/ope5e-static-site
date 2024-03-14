1d66ccc980e28fb966a51a38d05060e3e58d6999
sturlen <43607012+Sturlen@users.noreply.github.com>
2024-03-14 23:17:51 +0100

    test

--- api/search.ts
+++ api/search.ts
@@ -1,18 +1,11 @@
-import Fuse from "fuse.js"
-import Monsters from "../cache/monsters.json"
-
 export const prerender = false
-
-const fuse = new Fuse(Monsters)
 
 export default async (request: Request) => {
     const url = new URL(request.url)
     const name = url.searchParams.get("name") ?? "Creature"
 
     if (!name) return new Response("Query must not be empty", { status: 400 })
-    const monsters = Monsters.filter((monster) =>
-        monster.name.toLowerCase().includes(name.toLowerCase())
-    )
+    const monsters = []
 
     const entries = monsters
 
