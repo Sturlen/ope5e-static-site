42a843be83e48dde0ba59720b95f5830b06ce140
sturlen <43607012+Sturlen@users.noreply.github.com>
2024-03-14 23:12:03 +0100

    try json import

--- api/search.ts
+++ api/search.ts
@@ -1,13 +1,18 @@
-import { getCollection } from "astro:content"
+import Fuse from "fuse.js"
+import Monsters from "../cache/monsters.json"
 
 export const prerender = false
+
+const fuse = new Fuse(Monsters)
 
 export default async (request: Request) => {
     const url = new URL(request.url)
     const name = url.searchParams.get("name") ?? "Creature"
 
     if (!name) return new Response("Query must not be empty", { status: 400 })
-    const monsters = await getCollection("monsters")
+    const monsters = Monsters.filter((monster) =>
+        monster.name.toLowerCase().includes(name.toLowerCase())
+    )
 
     const entries = monsters
 
