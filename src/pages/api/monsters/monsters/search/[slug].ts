4eb57283a6ae88abfc93a55c2d4b8ace6fc0944a
sturlen <43607012+Sturlen@users.noreply.github.com>
2024-03-14 16:57:24 +0100

    add fuse search

---
+++ src/pages/api/monsters/monsters/search/[slug].ts
@@ -0,0 +1,16 @@
+import type { APIRoute } from "astro"
+import { getCollection, getEntry } from "astro:content"
+import Fuse from "fuse.js"
+
+const monsters = await getCollection("monsters")
+const fuse = new Fuse(monsters)
+
+export const GET: APIRoute = async ({ params, request }) => {
+    const slug = params.slug
+
+    if (!slug) return new Response("Query must not be empty", { status: 400 })
+
+    const entries = fuse.search(slug)
+
+    return new Response(JSON.stringify(entries, null, 4))
+}
