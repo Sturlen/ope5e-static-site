4bceeef8270f43d03747de193cc72236da186f47
sturlen <43607012+Sturlen@users.noreply.github.com>
2024-03-17 19:26:22 +0100

    log entries

--- api/search.ts
+++ api/search.ts
@@ -9,10 +9,11 @@ export default async (request: Request) => {
     const name = url.searchParams.get("name") ?? "Creature"
 
     if (!name) return new Response("Query must not be empty", { status: 400 })
+    const monster_url = "https://" + url.host + "/monsters.json"
+    console.log("monster_url", monster_url)
+    const entries: Promise<any[]> = await (await fetch(monster_url)).json()
 
-    const entries: Promise<any[]> = (
-        await fetch("https://" + url.host + "/monsters.json")
-    ).json()
+    console.log("ent", entries)
 
     return new Response(JSON.stringify(entries, null, 4))
 }
