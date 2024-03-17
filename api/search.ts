f9b7603587a8e4caabea95448dd3782845c10699
sturlen <43607012+Sturlen@users.noreply.github.com>
2024-03-17 20:27:13 +0100

    Revert "try fetching before"
    This reverts commit 5fdf99926f07fb670e6136fc928403a203c68e7b.

--- api/search.ts
+++ api/search.ts
@@ -6,18 +6,18 @@ export const config = {
     runtime: "edge",
 }
 
-const monster_url = "https://" + process.env.VERCEL_URL + "/monsters.json"
-const entries: any[] = await (await fetch(monster_url)).json()
-const fuse = new Fuse(entries, { keys: ["name"] })
-console.log("monster_url", monster_url)
-
-console.log("ent", entries)
-
 export default async (request: Request) => {
     const url = new URL(request.url)
     const name = url.searchParams.get("name") ?? "Creature"
 
     if (!name) return new Response("Query must not be empty", { status: 400 })
+    const monster_url = "https://" + process.env.VERCEL_URL + "/monsters.json"
+    console.log("monster_url", monster_url)
+    const entries: any[] = await (await fetch(monster_url)).json()
+
+    console.log("ent", entries)
+
+    const fuse = new Fuse(entries, { keys: ["name"] })
 
     return new Response(JSON.stringify(fuse.search(name), null, 4))
 }
